8ac3d550bcb3ba01d766259e6f2c9c30095aa119
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-05-19 00:26:43 +0300

    Update TypeORM to use DB_URL and SSL options

--- subscription-service/src/modules/orm/config/config.ts
+++ subscription-service/src/modules/orm/config/config.ts
@@ -5,11 +5,15 @@ dotenv.config();
 
 export const typeOrmModuleOptions: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST ,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST ,
+    // port: Number(process.env.DB_PORT),
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    url: process.env.DB_URL,
     synchronize: true,
     logging: false,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
